9bcc109af13b4e48df08af56ea7a92ddd49e324b
Emma Stockton <emmastockton10@gmail.com>
2020-06-21 12:08:33 +0100

    Add linting rules for consistancy
    -fix linting errors
    -add prettier
    -add pre-commit hook to check linting

--- client/src/reducers/RenderReducer.js
+++ client/src/reducers/RenderReducer.js
@@ -1,52 +1,55 @@
-import {setRender} from '../sharedCode';
-import {storeData} from '../API/StoreData';
+import { setRender } from "../sharedCode";
+import { storeData } from "../API/StoreData";
 
 export const renderReducer = (state, action) => {
   switch (action.type) {
-    case 'StoreQuiz':
+    case "StoreQuiz": {
       console.log("StoreQuiz");
       state.completed = action.update.completed;
-      let answers = createResponse(state);
+      const answers = createResponse(state);
       console.log(answers);
 
       storeData(answers);
 
       return state;
-    case 'AnswerQuestion':
+    }
+    case "AnswerQuestion": {
       state.answers.set(state.currentQuestionId, action.update.answer);
       const currentQuestion = state.questionMap.get(state.currentQuestionId);
 
       // Move to the next question
-      state.currentQuestionId = currentQuestion.nextQuestionId 
-                               ? currentQuestion.nextQuestionId : state.answerMap.get(action.update.answer).nextQuestionId;
-  
+      state.currentQuestionId = currentQuestion.nextQuestionId
+        ? currentQuestion.nextQuestionId
+        : state.answerMap.get(action.update.answer).nextQuestionId;
+
       // call re-render
       const nextQuestion = state.questionMap.get(state.currentQuestionId);
-      let nextRender = setRender(nextQuestion);
+      const nextRender = setRender(nextQuestion);
       action.update.nextPage(nextRender);
 
       return state;
+    }
     default:
       return state;
   }
-}
+};
 
 const createResponse = (state) => {
   return {
     quizId: "" + state.quizId,
-    questionAnswers: createAnswerObject(state.answers)
-  }
-}
+    questionAnswers: createAnswerObject(state.answers),
+  };
+};
 
 const createAnswerObject = (answerMap) => {
-  let results = [];
+  const results = [];
 
   answerMap.forEach((value, key) => {
-    let question = {};
-    question.id="" + key;
+    const question = {};
+    question.id = "" + key;
     question.answers = Array.isArray(value) ? value : ["" + value];
     results.push(question);
   });
 
   return results;
-}
+};
