438feb553cab15a04e3e591152dfdf6cf9b16e59
Emma Stockton <emmastockton10@gmail.com>
2020-06-22 15:47:38 +0100

    remove unneeded key prop from button

--- client/src/components/RenderButton.js
+++ client/src/components/RenderButton.js
@@ -4,11 +4,10 @@ import React from "react";
 const RenderButton = ({ question, isLoading, onAnswer }) => {
   const AnswerButton = ({ answer }) => {
     return (
-      <Row key={answer.id} xs="12" style={{ margin: "10px 0" }}>
+      <Row xs="12" style={{ margin: "10px 0" }}>
         <Col>
           <Button
             disabled={isLoading}
-            key={answer.id}
             color="primary"
             block
             onClick={() => {
